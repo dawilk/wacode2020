d666aa7295ef50cf3539a2e8f17fadc19ffe0305
Daniel <>
2020-02-22 13:17:32 -0600

    update app to define db explicitly in config

--- index.js
+++ index.js
@@ -14,35 +14,30 @@ let data;
 
 const loadDatabase = () => {
   console.log('Loading database...');
-  // process files in config.path
-  fs.readdir(config.path, function (err, items) {
-    for (let i = 0; i < items.length; i++) {
+  // load database file at config.database_path
+  const file = config.database_path;
+  // console.log('Start: ' + file);
 
-      const file = config.path + '/' + items[i];
-      // console.log('Start: ' + file);
-
-      // read file stats (if we want to console them)
-      fs.stat(file, function (err, stats) {
-        // console.log(file);
-        // console.log(stats['size']);
-      });
-
-      // load contents to object
-      data = JSON.parse(fs.readFileSync(file).toString());
-      // validate object
-      // console.log(contents);
-
-      // begin comparison loop/instruction
-
-      // remove file
-      //   fs.unlink(file, function(err) {
-      //     if (err) throw err;
-      //     // if no error, file has been deleted successfully
-      //     // console.log(`Deleted: ${file}`);
-      //   });
-      console.log(data[0].model);
-    }
+  // read file stats (if we want to console them)
+  fs.stat(file, function (err, stats) {
+    // console.log(file);
+    // console.log(stats['size']);
   });
+
+  // load contents to object
+  data = JSON.parse(fs.readFileSync(file).toString());
+  // validate object
+  // console.log(contents);
+
+  // begin comparison loop/instruction
+
+  // remove file
+  //   fs.unlink(file, function(err) {
+  //     if (err) throw err;
+  //     // if no error, file has been deleted successfully
+  //     // console.log(`Deleted: ${file}`);
+  //   });
+  console.log(data[0].model);
 };
 
 
